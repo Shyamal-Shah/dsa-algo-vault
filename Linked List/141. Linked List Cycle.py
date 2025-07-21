092a082a65ae231d6a724afd6430cfb6a586d99a
Shyamal Shah <shahshyamal35@gmail.com>
2025-07-21 22:29:11 +0530

    Add implementation for Linked List Cycle detection algorithm

---
+++ Linked List/141. Linked List Cycle.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+
+        return False
+
+
+if __name__ == "__main__":
+    head = ListNode(3)
+    head.next = ListNode(2)
+    head.next.next = ListNode(0)
+    head.next.next.next = ListNode(-4)
+    head.next.next.next.next = head.next
+    print(Solution().hasCycle(head))
